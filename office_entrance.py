d5a766966cde1dc58ae92d9ed8ea165e3b3a9766
8059909-cpu <8059909@student.harmonytx.org>
2026-02-13 11:44:58 -0700

    Create office_entrance.py with entrance defense system classes

---
+++ office_entrance.py
@@ -0,0 +1,54 @@
+class Entrance:
+    def __init__(self):
+        # Initialize entrance attributes
+        pass
+
+    def open(self):
+        # Logic to open the entrance
+        pass
+
+    def close(self):
+        # Logic to close the entrance
+        pass
+
+
+class FreddyMask:
+    def __init__(self):
+        # Initialize Freddy's mask attributes
+        pass
+
+    def wear(self):
+        # Logic to wear the mask
+        pass
+
+    def remove(self):
+        # Logic to remove the mask
+        pass
+
+
+class SilentVentilation:
+    def __init__(self):
+        # Initialize ventilation attributes
+        pass
+
+    def activate(self):
+        # Logic to activate silent ventilation
+        pass
+
+    def deactivate(self):
+        # Logic to deactivate silent ventilation
+        pass
+
+
+class OfficeEntranceSystem:
+    def __init__(self):
+        # Initialize the associated system attributes
+        pass
+
+    def secure(self):
+        # Logic to secure the office entrance
+        pass
+
+    def breach(self):
+        # Logic to breach the office entrance defense
+        pass
